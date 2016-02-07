71e50f3159316e6f3e42fcb40846c8b127711832
root <root@WordPress-on-1404.122115-172>
2016-02-07 07:36:11 -0500

    mailchimp js

--- wp-content/themes/barcelona-child/functions.php
+++ wp-content/themes/barcelona-child/functions.php
@@ -9,7 +9,6 @@ add_action( 'after_setup_theme', 'barcelona_child_theme_scripts', 99 );
 function barcelona_child_theme_scripts() {
 
 	add_action( 'wp_enqueue_scripts', 'barcelona_enqueue_scripts_child', 99 );
-	add_filter( 'ot_show_pages', '__return_true' );
 }
 
 /*
@@ -22,12 +21,9 @@ function barcelona_enqueue_scripts_child() {
 		wp_register_style( 'barcelona-main-child', trailingslashit( get_stylesheet_directory_uri() ).'style.css', array(), BARCELONA_THEME_VERSION, 'all' );
 		wp_enqueue_style( 'barcelona-main-child' );
 
+		wp_enqueue_script( 'mailchimp', '//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js', array(), false, true);
+		wp_enqueue_script( 'bf-main', get_stylesheet_directory_uri().'/main.js', array(), false, true);
+
 	}
 
-}
-
-add_action( 'init', 'custom_init', 99 );
-
-function custom_init() {
-	add_filter( 'ot_show_pages', '__return_true' );
 }
