e2dc7acb0a44fa33f12b60105e4c49e2d1ea8cae
Ore Landau <Ore4444@gmail.com>
2016-03-30 12:47:33 +0300

    MANUAL

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,15 @@ git add -A
 git commit -m "MANUAL"
 git push
 
-ssh dms #DMag Staging
+#DMag Staging
+ssh dms << REMOTE
+	cd /var/www/html
+	git pull
+	#MANUAL: enter GitHub credentials
+	#MANUAL: enter GitHub credentials for submodule
 
-cd /var/www/html
-git pull
-#MANUAL: enter GitHub credentials
-#MANUAL: enter GitHub credentials for submodule
-
-cd/var/www/html/wp-content/plugins/d-magazine
-npm install
-bower install --allow-root
-gulp
-
-exit
+	cd /var/www/html/wp-content/plugins/d-magazine
+	npm install
+	bower install --allow-root
+	gulp
+REMOTE
