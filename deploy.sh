5bfd968fd19c6c7cd46a1fb99cf2dca90f963eaf
Ore Landau <Ore4444@gmail.com>
2016-03-30 13:10:40 +0300

    MANUAL

--- deploy.sh
+++ deploy.sh
@@ -14,11 +14,9 @@ git push
 ssh dms << REMOTE
 	cd /var/www/html
 	git pull
-	#MANUAL: enter GitHub credentials
-	#MANUAL: enter GitHub credentials for submodule
 
 	cd /var/www/html/wp-content/plugins/d-magazine
 	npm install
-	bower install --allow-root
+	bower install
 	gulp
 REMOTE
