2141be583f58c0204bc6742468d6cf4ace683de1
Ore Landau <Ore4444@gmail.com>
2016-03-30 12:42:22 +0300

    MANUAL

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd /c/WTServer/WWW/magazine/wp-content/plugins
+git add -A
+git commit -m "MANUAL"
+git push
+
+cd /c/WTServer/WWW/magazine
+git add -A
+git commit -m "MANUAL"
+git push
+
+ssh dms #DMag Staging
+
+cd /var/www/html
+git pull
+#MANUAL: enter GitHub credentials
+#MANUAL: enter GitHub credentials for submodule
+
+cd/var/www/html/wp-content/plugins/d-magazine
+npm install
+bower install --allow-root
+gulp
+
+exit
