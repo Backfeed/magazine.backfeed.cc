0906184eb5294d3653c55bd44e2533ea082c7e0c
Ore Landau <Ore4444@gmail.com>
2016-04-04 17:55:25 +0300

    fix for homepage when protocol is not connected

--- wp-content/themes/barcelona-child/lib/ajax.php
+++ wp-content/themes/barcelona-child/lib/ajax.php
@@ -17,25 +17,28 @@ function backfeed_get_module_posts() {
 
         $paged = (array_key_exists('barcelona_paged', $_POST) && is_numeric($_POST['barcelona_paged'])) ? $_POST['barcelona_paged'] : 1;
 
-        $contributions = Api::get_all_contributions();
-        usort($contributions, function($a, $b) { return $b->score - $a->score; });
-        $contribution_ids = array_column($contributions, 'id');
-
         $barcelona_q = new WP_Query([
             'posts_per_page'        => 8,
             'post_type'             => 'post',
             'post_status'           => 'publish',
             'ignore_sticky_posts'   => true,
             'no_found_rows'         => false,
-            'paged'                 => $paged,
-            'meta_query' => [
+            'paged'                 => $paged
+        ]);
+
+        $contributions = Api::get_all_contributions();
+
+        if (is_array($contributions)) {
+            usort($contributions, function($a, $b) { return $b->score - $a->score; });
+            $contribution_ids = array_column($contributions, 'id');
+            $barcelona_q->meta_query = [
                 [
                     'key' => 'backfeed_contribution_id',
                     'value' => $contribution_ids,
                     'compare' => 'IN'
                 ]
-            ]
-        ]);
+            ];
+        }
 
         include(locate_template('bunch-of-articles.php'));
     }
