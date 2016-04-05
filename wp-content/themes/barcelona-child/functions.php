c984cb52ac6405a06a45288d04f595ceead4e7f4
Ore Landau <Ore4444@gmail.com>
2016-04-05 15:48:49 +0300

    raw space should ignore sticky post

--- wp-content/themes/barcelona-child/functions.php
+++ wp-content/themes/barcelona-child/functions.php
@@ -42,5 +42,5 @@ add_filter('gform_rich_text_editor_options', function($editor_settings) {
 });
 
 add_action('init', function() {
-	add_rewrite_rule('^raw-space$', 'index.php?post_type=post&orderby=date', 'top');
+	add_rewrite_rule('^raw-space$', 'index.php?post_type=post&orderby=date&ignore_sticky_posts=true', 'top');
 });
